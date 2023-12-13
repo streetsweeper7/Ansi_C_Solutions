cd7f3b9e4f09d8cca5452ded6ee7212970621090
jbanda <adrielbanda4@gmail.com>
2023-12-13 22:36:05 +0000

    Ex 1-7

---
+++ source/ch1/ex_1_7.c
@@ -0,0 +1,14 @@
+/* Exercise 1-7
+ * Authoer: John Banda
+ * Desciption: Print the value of EOF
+ */
+
+#include <stdio.h>
+
+int main(){
+    // Print the value of EOF
+    printf("The value of EOF is: %d\n", EOF);
+
+    return 0;
+
+}
